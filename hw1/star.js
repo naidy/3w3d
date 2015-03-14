fbca935a1f985e1efe0fc5f8f171c165451d2ed3
naidy <allen830523@gmail.com>
2015-03-15 00:14:29 +0800

    3/15/2015

---
+++ hw1/star.js
@@ -0,0 +1,40 @@
+var star = false;
+var particleSystem;
+var star_shine = 1.0;  // unfinished
+
+function initStar()
+{
+	var particleCount = 1800,
+		particles = new THREE.Geometry(),
+		pMaterial = new THREE.ParticleBasicMaterial({
+			size: 10,
+			color: 0xffffff,
+			map: THREE.ImageUtils.loadTexture(
+				"images/star_2.jpg"
+			),
+			blending: THREE.AdditiveBlending,
+			transparent: true
+		});
+		
+	for (var p = 0; p < particleCount; p++)
+	{
+		var pX = Math.random() * 2000 - 1000; pX>0?pX+=100:pX-=100;
+		var pY = Math.random() * 2000 - 1000; pY>0?pY+=100:pY-=100;
+		var pZ = Math.random() * 2000 - 1000; pZ>0?pZ+=100:pZ-=100;
+		var particle = new THREE.Vector3 (pX, pY, pZ);
+		particles.vertices.push(particle);
+	}
+	
+	particleSystem = THREE.ParticleSystem(
+		particles, pMaterial);
+}
+
+function createStar()
+{
+	scene.add (particleSystem);
+}
+
+function removeStar()
+{
+	scene.remove (particleSystem);
+}
